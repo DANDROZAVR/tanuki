02e87c911739109378a25f1e29a6e4f7c8bcfb6c
chmurka1 <dominik06252001@gmail.com>
2023-04-17 01:40:56 +0200

    Add gui improvements + running script functionality

--- client/src/renderer/App.tsx
+++ client/src/renderer/App.tsx
@@ -1,10 +1,8 @@
 import './App.css';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import Editor from '@monaco-editor/react';
 import React from 'react';
-import sendScript from './network/client.ts';
+import { sendScript, execScript } from './network/client.ts';
 import 'reactflow/dist/style.css';
-import icon from '../../assets/icon.svg';
 
 function App() {
   const [lastScriptName, setLastScriptName] = React.useState('');
@@ -16,7 +14,7 @@ function App() {
 
   return (
     <div>
-      <div className="container">
+      <section className="form-section">
         <div id="text-editor">
           <Editor
             height="50vh"
@@ -25,8 +23,6 @@ function App() {
             onMount={onEditorMount}
           />
         </div>
-      </div>
-      <div className="container">
         <button
           type="button"
           onClick={() => {
@@ -36,58 +32,20 @@ function App() {
         >
           Send
         </button>
-      </div>
+      </section>
+      <section className="form-section">
+        <input type="text" placeholder="script1-1-2001-1-1-1.tnk" />
+        <button
+          type="button"
+          onClick={() => {
+            execScript(lastScriptName);
+          }}
+        >
+          Run script
+        </button>
+      </section>
     </div>
   );
 }
-
-/*
-function Hello() {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-}
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
-}
-*/
 
 export default App;
--- client/src/renderer/network/client.ts
+++ client/src/renderer/network/client.ts
@@ -1,4 +1,4 @@
-export default function sendScript(ref: string) {
+export function sendScript(ref: string) {
   const script = ref.current.getValue();
   const timestamp = new Date();
   const scriptName = `script${timestamp.getDay()}-${timestamp.getMonth()}-${timestamp.getFullYear()}-${timestamp.getHours()}-${timestamp.getMinutes()}-${timestamp.getSeconds()}.tnk`;
@@ -10,8 +10,7 @@ export default function sendScript(ref: string) {
   connection.open('POST', url, true);
   connection.onreadystatechange = function onStateChange() {
     if (connection.readyState === 4 && connection.status === 200) {
-      alert(`Sent ${scriptName}`);
-      console.log(connection.responseText);
+      alert(`Sent script with name ${scriptName}`);
     }
   };
   connection.setRequestHeader('Content-type', 'application/json');
@@ -25,4 +24,27 @@ export default function sendScript(ref: string) {
   );
 
   return scriptName;
+}
+
+export function execScript(ref: string) {
+  const scriptName = ref.current.getValue();
+
+  /* establish http connection */
+  const connection = new XMLHttpRequest();
+  // TODO: custom server address
+  const url = 'http://localhost:3001';
+  connection.open('POST', url, true);
+  connection.onreadystatechange = function onStateChange() {
+    if (connection.readyState === 4 && connection.status === 200) {
+      alert(`Run script ${scriptName}`);
+    }
+  };
+  connection.setRequestHeader('Content-type', 'application/json');
+  connection.send(
+    JSON.stringify({
+      type: 'exec-script',
+      user: 'admin',
+      title: scriptName,
+    })
+  );
 }
